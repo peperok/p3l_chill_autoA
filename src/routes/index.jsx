8192a8d8cfb27c757cf673cb24bf4cd0833a101f
abcd9206 <220711768@students.uajy.ac.id>
2025-05-13 01:14:06 +0700

    login oke 1

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeBefore from "../components/Home/HomeBefore";
 import ProfilPenitip from "../components/Profil/ProfilPenitip";
 import ProfilPembeli from "../components/Profil/ProfilPembeli"; 
+import FormLogin from "../components/Form/FormLogin";
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/homebefore" element={<HomeBefore />} />
+        <Route path="/" element={<HomeBefore />} />
         <Route path="/profilpenitip" element={<ProfilPenitip />} />
         <Route path="/profilpembeli" element={<ProfilPembeli />} />
+        <Route path="/login" element={<FormLogin />} />
       </Routes>
     </Router>
   );
