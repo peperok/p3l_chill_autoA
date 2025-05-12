d290c88908661314894fd4935414191f9a113f0e
peperok <220711948@students.uajy.ac.id>
2025-05-13 05:27:19 +0700

    gjd

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,21 @@
 
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeBefore from "../components/Home/HomeBefore";
+import HomeAfter from "../components/Home/HomeAfter";
 import ProfilPenitip from "../components/Profil/ProfilPenitip";
-import ProfilPembeli from "../components/Profil/ProfilPembeli"; // pastikan file ini ada
+import ProfilPembeli from "../components/Profil/ProfilPembeli";
+//import FormLogin from "../components/Form/FormLogin";
+
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/homebefore" element={<HomeBefore />} />
+        <Route path="/" element={<HomeBefore />} />
+        <Route path="/homeafter" element={<HomeAfter />} />
         <Route path="/profilpenitip" element={<ProfilPenitip />} />
         <Route path="/profilpembeli" element={<ProfilPembeli />} />
+        //<Route path="/login" element={<FormLogin/>}/>
       </Routes>
     </Router>
   );
