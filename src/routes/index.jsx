e934d670b3a664e870572f122b5f96f39a147e84
peperok <220711948@students.uajy.ac.id>
2025-05-13 07:44:16 +0700

    homeadmin+folderadmin+index

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,12 +3,14 @@ import HomeBefore from "../components/Home/HomeBefore";
 import ProfilPenitip from "../components/Profil/ProfilPenitip";
 import ProfilPembeli from "../components/Profil/ProfilPembeli"; 
 import FormLogin from "../components/Form/FormLogin";
+import HomeAdmin from "../components/Home/HomeAdmin";
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomeBefore />} />
+        <Route path="/admin" element={<HomeAdmin />} />
         <Route path="/profilpenitip" element={<ProfilPenitip />} />
         <Route path="/profilpembeli" element={<ProfilPembeli />} />
         <Route path="/login" element={<FormLogin />} />
